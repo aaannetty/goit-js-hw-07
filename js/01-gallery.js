2c0d1d70da519ba5152f8668169362d6fbad4dd6
aaannetty <latsdoit@gmail.com>
2023-05-02 00:08:28 +0300

    Gallery created

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+function createGalleryItemsMarkup(items) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return ` <li class="gallery__item"> <a class="gallery__link" href = "${original}">
+  <img "class=gallery__image" src = "${preview}" data-source = "${original}" alt = "${description}"/> </a>
+  </li> 
+`;
+    })
+    .join('');
+}
+galleryList.insertAdjacentHTML('beforeend', createGalleryItemsMarkup(galleryItems));
